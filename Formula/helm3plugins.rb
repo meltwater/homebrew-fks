8be1eb47d578a9fcd6d0d47807665b879ee82790
Simone Sciarrati <s.sciarrati@gmail.com>
2021-03-18 11:38:06 +0100

    test

--- Formula/helm3plugins.rb
+++ Formula/helm3plugins.rb
@@ -8,8 +8,8 @@ class Helm3plugins < Formula
   sha256 "451ad70dfe286e3979c78ecf7074f4749d93644da8aa2cc778e2f969771f1794"
 
   def install
-    system "/usr/local/bin/helm3 plugin install https://github.com/databus23/helm-diff --version v3.1.3"
     system "/usr/local/bin/helm3 plugin install https://github.com/jkroepke/helm-secrets --version v3.5.0"
+    system "/usr/local/bin/helm3 plugin install https://github.com/databus23/helm-diff --version v3.1.3"
     system "/usr/local/bin/helm3 plugin install https://github.com/aslafy-z/helm-git --version v0.10.0"
   end
 
