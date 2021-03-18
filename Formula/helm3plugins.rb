5fed3d0e511b47aadaf1488537d6225b41f06a30
Simone Sciarrati <s.sciarrati@gmail.com>
2021-03-18 11:25:17 +0100

    Plit helm and plugins formula

--- Formula/helm3plugins.rb
+++ Formula/helm3plugins.rb
@@ -1,12 +1,14 @@
-class Helm3 < Formula
+class Helm3plugins < Formula
   version "3.5.3"
-  desc "Helm is a tool for managing Charts. Charts are packages of pre-configured Kubernetes resources."
+  desc "Helm plugins."
   homepage "https://github.com/meltwater/homebrew-fks"
   url "https://github.com/meltwater/homebrew-fks/releases/download/helm3-v#{version}/helm-darwin-amd64.tar.gz"
   sha256 "451ad70dfe286e3979c78ecf7074f4749d93644da8aa2cc778e2f969771f1794"
 
   def install
-    bin.install "helm" => "helm3"
+    system "/usr/local/bin/helm3 plugin install https://github.com/databus23/helm-diff --version v3.1.3"
+    system "/usr/local/bin/helm3 plugin install https://github.com/jkroepke/helm-secrets --version v3.5.0"
+    system "/usr/local/bin/helm3 plugin install https://github.com/aslafy-z/helm-git --version v0.10.0"
   end
 
   # Homebrew requires tests.
