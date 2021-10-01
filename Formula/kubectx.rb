d8295ee7711b8317adf45999453bacc934835b71
Rasmus Johansson <rasmus.johansson@meltwater.com>
2021-10-01 09:57:04 +0200

    new sha for kubectx

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -3,7 +3,7 @@ class Kubectx < Formula
   desc "Kubectx is a utility to manage and switch between kubectl contexts. Kubens is a utility to switch between Kubernetes namespaces."
   homepage "https://github.com/meltwater/homebrew-fks"
   url "https://github.com/meltwater/homebrew-fks/releases/download/kubectx-v#{version}/kubectx_v#{version}_darwin.tar.gz"
-  sha256 "8bd3b090ebafa87f5838e52fdd4f6faa603df2dd307af384c232eb6cd43b3eee"
+  sha256 "b1ef9adcca620181b784dbd84fd154ad29d7ae11231babc77fb80f1e8da77206"
 
   def install
     bin.install "kubectx"
