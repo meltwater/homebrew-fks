45e19bf8cc45c9c5a77e0019fd665bcf3a799205
Federico Hernandez <f@ederi.co>
2021-11-23 14:41:22 +0100

    Bravo kops

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -1,9 +1,9 @@
 class Kops < Formula
-  version "1.18.2"
+  version "1.18.2-bravo"
   desc "The Kops command-line tool, kops, allows you to set up Kubernetes clusters."
   homepage "https://github.com/meltwater/homebrew-fks"
   url "https://github.com/meltwater/homebrew-fks/releases/download/kops-#{version}/kops-darwin-amd64.tar.gz"
-  sha256 "105bfa7c1dde56d8b3a6a836e64ff632ae03a0785515f9842589b1aa7801486a"
+  sha256 "3b3ae9ed6053756f745b70115f1612d74556e9f8c13979a44573eee330c1e2df"
 
   def install
     bin.install "kops"
@@ -11,6 +11,6 @@ class Kops < Formula
 
   # Homebrew requires tests.
   test do
-    assert_match "#{version}", shell_output("#{bin}/kops version", 0)
+    assert_match "git-32856aa2f8-Bravo", shell_output("#{bin}/kops version", 0)
   end
 end
