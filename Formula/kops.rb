bc3be3b57f3743a98a969488c839d81c80a1607e
Federico Hernandez <f@ederi.co>
2023-03-29 13:51:33 +0200

    Adds spok 1.22.5 to kops package

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -1,16 +1,18 @@
 class Kops < Formula
-  version "1.18.2-bravo"
+  version "1.18.2-bravo-2"
   desc "The Kops command-line tool, kops, allows you to set up Kubernetes clusters."
   homepage "https://github.com/meltwater/homebrew-fks"
   url "https://github.com/meltwater/homebrew-fks/releases/download/kops-#{version}/kops-darwin-amd64.tar.gz"
-  sha256 "3b3ae9ed6053756f745b70115f1612d74556e9f8c13979a44573eee330c1e2df"
+  sha256 "bbf5b5ddcf5f7224537f4120aa76dce8b87735ea04c22298f1132a94ef03ea35"
 
   def install
     bin.install "kops"
+    bin.install "spok"
   end
 
   # Homebrew requires tests.
   test do
     assert_match "git-32856aa2f8-Bravo", shell_output("#{bin}/kops version", 0)
+    assert_match "git-3a6f2117f322bfc37da814bde1feb12dabc943f2", shell_output("#{bin}/spok version", 0)
   end
 end
