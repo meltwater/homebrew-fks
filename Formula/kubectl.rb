e747553543059f6ce3ee36cce079b795030aab19
Federico Hernandez <f@ederi.co>
2023-11-10 10:04:57 +0100

    kubectl v1.21.14

--- Formula/kubectl.rb
+++ Formula/kubectl.rb
@@ -1,9 +1,18 @@
 class Kubectl < Formula
-  version "1.18.9"
+  version "1.21.14"
   desc "The Kubernetes command-line tool, kubectl, allows you to run commands against Kubernetes clusters."
   homepage "https://github.com/meltwater/homebrew-fks"
-  url "https://github.com/meltwater/homebrew-fks/releases/download/kubectl-v#{version}/kubectl-darwin-amd64.tar.gz"
-  sha256 "65f2606a4291bfbd474a2c7fe16572726a42be6e98582f521283f56df89c27e7"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/meltwater/homebrew-fks/releases/download/kubectl-v#{version}/kubectl-darwin-amd64.tar.gz"
+      sha256 "348ad2075e0c07d4210a760706b3d0892766dfbe622a120827648a4ce1669c25"
+    end
+    on_arm do
+      url "https://github.com/meltwater/homebrew-fks/releases/download/kubectl-v#{version}/kubectl-darwin-arm64.tar.gz"
+      sha256 "88df2ca27da441da4c23427304b5206a51f25137e6c14056e7819a0ff9c57f7a"
+    end
+  end
 
   def install
     bin.install "kubectl"
